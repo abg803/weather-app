7f6e1b5c2ea9bdec6b9f59aab9a0e17f390593b6
abg803 <98655951+abg803@users.noreply.github.com>
2022-01-30 21:38:01 -0500

    updated description id

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function showWeather(response) {
   );
 
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
